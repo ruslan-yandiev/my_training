0ecb55faa29d6baa8ae072add1d3c3a97947c676
ruslan <sqwer1986@gmail.com>
2020-05-10 14:27:40 +0300

    create rack interface

---
+++ new_rack/app.rb
@@ -0,0 +1,35 @@
+require_relative 'formatter'
+
+class App
+  def call(env)
+    perform_response(env)
+  end
+
+  def perform_response(env)
+    request = Rack::Request.new(env)
+
+    path = request.path
+    request_params = request.params['format']
+
+    if path != '/ruby'
+      return [404, headers, ["404\n"]]
+    elsif request_params.nil?
+      return [400, headers, ["invalid_format_name\n"]]
+    end
+  end
+
+  formatter = Formatter.new(request_params)
+
+  if formatter.valid?
+    body = formatter.extension
+    status = 200
+  else
+    invalid_params = formatter.invalid_params
+    status = 400
+  end
+  [status, headers, ["С расширением #{ormatter::EXTENSION} всего файлов: #{body}\n"]]
+
+  def headers
+    { 'Content-Type' => 'text/plain' }
+  end
+end
---
+++ new_rack/formatter.rb
@@ -0,0 +1,26 @@
+class Formatter
+
+  EXTENSION = 'rb'
+
+  attr_accessor :params
+
+  def initialize(params)
+    @params = params
+  end
+
+  def extension
+    body = number_of_files(@params)
+  end
+
+  def invalid_params
+    @params
+  end
+
+  def valid?
+    Formatter::EXTENSION == @params
+  end
+
+  def number_of_files(params)
+    Dir.glob("../**/*.#{params}").size
+  end
+end
