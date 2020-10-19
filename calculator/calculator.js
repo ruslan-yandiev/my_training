0687e5fb98b734042347a84a5a6ad090d6ee1e15
ruslan <sqwer1986@gmail.com>
2020-10-19 23:06:49 +0300

    update

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -5,45 +5,50 @@ let arr = ["", "", ""];
 let count = 0;
 
 document.querySelector(".interface").addEventListener("click", (event) => {
-    const buttonContent = event.target.textContent;
-    
-    if (buttonContent === "C") {
-      arr[0] = '';
-      arr[1] = '';
-      arr[2] = '';
-      count = 0;
-      show("");
-      return;
+  const buttonContent = event.target.textContent;
+  show(buttonContent);
+  if (buttonContent === "C") {
+    arr[0] = '';
+    arr[1] = '';
+    arr[2] = '';
+    count = 0;
+    show("");
+    return;
+  }
+
+
+  if (buttonContent !== "=" && buttonContent.length === 1 && event.target.className === "button") {
+    setColor(event);
+
+    if (buttonContent === "*" || buttonContent === "/" || buttonContent === "+" || buttonContent === "-") {
+      if (arr[2]) { arr[1] = ''; }
+      arr[2] = buttonContent;
+      count = 1;
+      show(arr[2]);
+    } else if (buttonContent !== "*" || buttonContent !== "/" || buttonContent !== "+" || buttonContent !== "-") {
+      arr[count] = arr[count] + buttonContent;
+      show(arr[count]);
     }
+  }
 
-
-    if (buttonContent !== "=" && buttonContent.length === 1 && event.target.className === "button") {
-      setColor(event);
-      show(buttonContent);
-
-      if (buttonContent === "*" || buttonContent === "/" || buttonContent === "+" || buttonContent === "-") {
-        if (arr[2]) { arr[1] = ''; }
-        arr[2] = buttonContent;
-        count = 1;
-      } else if (buttonContent !== "*" || buttonContent !== "/" || buttonContent !== "+" || buttonContent !== "-") {
-        arr[count] = arr[count] + buttonContent;
-      }
-    }
-
-    if (buttonContent === "=") {
-      myMoth(+arr[0], +arr[1], arr[2]);
-    }
-  });
+  if (buttonContent === "=") {
+    myMath(+arr[0], +arr[1], arr[2]);
+  }
+});
 
 function show(arg) {
   if (arg === "") {
     shows.textContent = "";
+  } else if (arg === "+" || arg === "-" || arg === "*" || arg === "/" ) {
+    show("");
+    shows.textContent = arg;
   } else {
+    show("");
     shows.textContent = shows.textContent + arg;
   }
 }
 
-function myMoth(number1, number2, operator) {
+function myMath(number1, number2, operator) {
   let result = 0;
 
   if (operator === "+") {
@@ -60,6 +65,7 @@ function myMoth(number1, number2, operator) {
   }
 
   arr[0] = result;
+  count = 0;
   show("");
   show(result);
 }
