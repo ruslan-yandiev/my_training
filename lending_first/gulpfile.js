408e8c1ceb82bab57140ac3783c3da392109ccd9
ruslan <sqwer1986@gmail.com>
2020-10-23 09:01:26 +0300

    create page

--- lending_first/gulpfile.js
+++ lending_first/gulpfile.js
@@ -1,19 +1,23 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
 
 function style() {
-  return gulp.src("./scss/**/*.scss").pipe(sass()).pipe(gulp.dest('./css')).pipe(browserSync.stream());
+    return gulp
+        .src("./scss/**/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./css"))
+        .pipe(browserSync.stream());
 }
 
 function watch() {
-	browserSync.init({
-		server: {
-			baseDir: './'
-		}
-	})
-	gulp.watch("./scss/**/*.scss", style);
-	gulp.watch("./*.html").on('change', browserSync.reload);
+    browserSync.init({
+        server: {
+            baseDir: "./",
+        },
+    });
+    gulp.watch("./scss/**/*.scss", style);
+    gulp.watch("./*.html").on("change", browserSync.reload);
 }
 
 exports.style = style;
