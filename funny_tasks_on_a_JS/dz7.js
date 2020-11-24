766381fef827d66dffd42c374565082dcae94e31
ruslan <sqwer1986@gmail.com>
2020-11-24 20:06:14 +0300

    add task

--- funny_tasks_on_a_JS/dz7.js
+++ funny_tasks_on_a_JS/dz7.js
@@ -46,3 +46,154 @@ function myCalc(num) {
 console.log(myCalc(100)); // 648
 
 // * ======================================================================================
+/*
+Функция принимает двумерный массив в виде треугольника (горки), необходимо найти наибольшую сумму от вершины до низа
+треугольника. Массив может иметь очень большое количество вложенных массивов.
+Проверять на правильность массива не нужно. передается всегда правильно составленный массив.
+*/
+function maxSum(arr) {
+    let arrSize = arr.length;
+    let maxSum = 0;
+
+    function sortMerge(elemArr) {
+        const size = elemArr.length;
+
+        if (size > 1) {
+            const mid = Math.floor(size / 2);
+            const leftPart = elemArr.slice(0, mid);
+            const rightPart = elemArr.slice(mid);
+
+            sortMerge(leftPart);
+            sortMerge(rightPart);
+
+            let l = (r = k = 0);
+            const sizeLeft = leftPart.length;
+            const sizeRight = rightPart.length;
+
+            while (l < sizeLeft && r < sizeRight) {
+                if (leftPart[l] <= rightPart[r]) {
+                    elemArr[k] = leftPart[l];
+                    l++;
+                } else {
+                    elemArr[k] = rightPart[r];
+                    r++;
+                }
+
+                k++;
+            }
+
+            while (l < sizeLeft) {
+                elemArr[k] = leftPart[l];
+                l++;
+                k++;
+            }
+
+            while (r < sizeRight) {
+                elemArr[k] = rightPart[r];
+                r++;
+                k++;
+            }
+        }
+        return elemArr;
+    }
+
+    for (let i = 0; i < arrSize; i++) {
+        arr[i] = sortMerge(arr[i]).reverse();
+    }
+
+    for (let i = 0; i < arrSize; i++) {
+        maxSum += arr[i][0];
+    }
+
+    return maxSum;
+}
+
+console.log(maxSum([[1], [4, 8], [1, 5, 3]])); // 14
+console.log(maxSum([[1], [-3, -4], [2, 1, 9]])); //7
+
+// * ======================================================================================
+/*
+Функция принимает двумерный массив в виде треугольника (горки), необходимо найти наибольшую сумму от вершины до низа
+треугольника. С числа сверху мы можем переходить лишь на нижнее число и его соседей.
+Массив может иметь очень большое количество вложенных массивов.
+Проверять на правильность массива не нужно. передается всегда правильно составленный массив.
+*/
+function maxSum2(arr) {
+    let sum = 0;
+    let index = 0;
+    sum += arr[0][index];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (
+            arr[i][index - 1] === undefined &&
+            arr[i][index] >= arr[i][index + 1]
+        ) {
+            sum += arr[i][index];
+        } else if (
+            arr[i][index + 1] === undefined &&
+            arr[i][index] >= arr[i][index - 1]
+        ) {
+            sum += arr[i][index];
+        } else if (
+            arr[i][index] >= arr[i][index + 1] &&
+            arr[i][index] >= arr[i][index - 1]
+        ) {
+            sum += arr[i][index];
+        } else if (
+            arr[i][index - 1] !== undefined &&
+            arr[i][index + 1] !== undefined &&
+            arr[i][index - 1] >= arr[i][index + 1] &&
+            arr[i][index - 1] > arr[i][index]
+        ) {
+            sum += arr[i][index - 1];
+            index -= 1;
+        } else {
+            sum += arr[i][index + 1];
+            index += 1;
+        }
+    }
+    return sum;
+}
+
+console.log(maxSum2([[1], [4, 8], [1, 5, 3]])); // 14
+console.log(maxSum2([[1], [-3, -4], [2, 1, 9]])); //0
+
+// * ======================================================================================
+/*
+Дана матрица, содержащая как положительные, так и отрицательные элементы.
+Удвлить все ее столбцы, содержащие только положительные элементы
+*/
+const arr3 = [
+    [2, 55, 8, 10],
+    [-1, 4, -9, 1],
+    [2, 4, -3, 50],
+    [7, 9, 7, 108],
+];
+
+let detect = 0;
+
+for (let i = 0; i < arr3[0].length; i++) {
+    for (let j = 0; j < arr3.length; j++) {
+        if (arr3[j][i] < 0) {
+            detect += 1;
+        }
+    }
+
+    if (detect === 0) {
+        for (let a = 0; a < arr3.length; a++) {
+            arr3[a].splice(arr3[a].indexOf(arr3[a][i]), 1);
+        }
+    }
+
+    detect = 0;
+}
+
+console.log(arr3);
+/*
+[
+    [2, 8],
+    [-1, -9],
+    [2, -3],
+    [7, 7]
+]
+*/
