9a1e67d9953228c23b76c1f81b3caf847d48012c
ruslan <sqwer1986@gmail.com>
2020-12-25 18:59:14 +0300

    add menu

--- lending_2/lending_2/js/script.js
+++ lending_2/lending_2/js/script.js
@@ -34,16 +34,28 @@ document.querySelector('.user-header__icon').addEventListener('click', () => {
 
     // Скроем бургер меню при появлении меню 1
     document.querySelector('.menu__body').classList.toggle('_active', false);
+    // удалим у кнопки бургер меню класс _active, что позволит из крестика превратить обратно в бургер.
+    document.querySelector('.icon-menu').classList.toggle('_active', false);
+    // удалим блокировку со скрола контента
+    document.querySelector('body').classList.toggle('_lock', false);
 });
 
 // ! Закрытие меню 1 при нажетии любого места экрана кроме меню
 document.addEventListener('click', (e) => {
     if (e.target.offsetParent.parentElement.classList[1] !== 'user-header') {
-        // Скроем меню 1 при пояалении меню бургера
+        // удалим класс _active у элемента(тега)
         document
             .querySelector('.user-header__menu')
             .classList.toggle('_active', false);
     }
+
+    // // ! такой вариант тоже работает
+    // if (!e.target.closest('.user-header')) {
+    //     // удалим класс _active у элемента(тега)
+    //     document
+    //         .querySelector('.user-header__menu')
+    //         .classList.remove('_active');
+    // }
 });
 
 // ! Всплывающее меню (бургера)
@@ -51,4 +63,8 @@ const iconMenu = document.querySelector('.icon-menu');
 iconMenu.addEventListener('click', () => {
     iconMenu.classList.toggle('_active');
     document.querySelector('.menu__body').classList.toggle('_active');
+    // ! при клике будет блокировка скрола(прокрутки основного контента), при повторном нажатии клас _lock удалится и блокировка пропадет
+    document.querySelector('body').classList.toggle('_lock');
 });
+
+// TODO разобраться почему бургеру неполучается задать плавность превращения в крестик и обратно.
