4627627f8572f4d7be2f6b3945b7e9d791bd5528
Ruslan <57350793+ruslan-yandiev@users.noreply.github.com>
2021-02-03 22:30:40 +0300

    переписал отображение региона с js на html и css

--- lending_2/lending_2/js/script.js
+++ lending_2/lending_2/js/script.js
@@ -17,35 +17,36 @@ testWebP(function (support) {
     }
 });
 ;
-// Физический перенос элемента от одного родителя другому
-(function() {
-    action();
+// // Физический перенос элемента от одного родителя другому
+// (function() {
+//     action();
 
-    window.addEventListener("resize", throttler, false);
+//     window.addEventListener("resize", throttler, false);
     
-    let detectTimeout;
+//     let detectTimeout;
 
-    function throttler() {
+//     function throttler() {
 
-        // игнорировать события изменения размера, пока в очереди находится фактическое выполнение action.
-        if ( !detectTimeout ) {
-            action(); // выполнится сразу, затем задержка
-            detectTimeout = setTimeout(() => {
-            detectTimeout = null;
-            // action(); // выполнится после задержки
-            }, 55);
-        }
-    }
+//         // игнорировать события изменения размера, пока в очереди находится фактическое выполнение action.
+//         if ( !detectTimeout ) {
+//             action(); // выполнится сразу, затем задержка
+//             detectTimeout = setTimeout(() => {
+//             detectTimeout = null;
+//             // action(); // выполнится после задержки
+//             }, 55);
+//         }
+//     }
 
-    function action() {
-        let region = document.querySelector('.actions-header__region');
-        let menuBody = document.querySelector('.menu__body');
-        let actionsHeader = document.querySelector('.actions-header');
+//     function action() {
+//         let region = document.querySelector('.actions-header__region');
+//         let menuBody = document.querySelector('.menu__body');
+//         let actionsHeader = document.querySelector('.actions-header');
 
-        document.documentElement.clientWidth < 768 ? menuBody.append(region) : actionsHeader.prepend(region);
-    }
+//         document.documentElement.clientWidth < 768 ? menuBody.append(region) : actionsHeader.prepend(region);
+//     }
     
-}());;
+// }());
+// ! отключил, так-как можно реализовать подобное на чистом html и css;
 
 // ! Всплывающее меню 1
 document.querySelector('.user-header__icon').addEventListener('click', () => {
