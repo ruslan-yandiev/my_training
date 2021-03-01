7a7846b4fc17b4b32ea94c7d1a95942de186c92d
ruslan <sqwer1986@gmail.com>
2021-03-02 02:02:14 +0300

    ...

--- funny_tasks_on_a_JS/dist/script.dev.js
+++ funny_tasks_on_a_JS/dist/script.dev.js
@@ -42,3 +42,19 @@ console.log(check('|(|)', [['(', ')'], ['|', '|']]), false); // -> false
 console.log(check('|()|(||)||', [['(', ')'], ['|', '|']]), true); // -> true
 
 console.log(check('|(||||(||)||)|', [['(', ')'], ['|', '|']]), true); // -> true
+// // Дано неупорядоченный массив целых чисел и значение sum. Верните true,
+// // если сумма любых двух элементов равняется значению sum. В противном случае верните false.
+// const findSum = (arr, val) => {
+//     let searchValues = new Set();
+//     searchValues.add(val - arr[0]);
+//     for (let i = 1, length = arr.length; i < length; i++) {
+//         let searchVal = val - arr[i];
+//         if (searchValues.has(arr[i])) {
+//             return true;
+//         } else {
+//             searchValues.add(searchVal);
+//         }
+//     }
+//     return false;
+// };
+// const findSum = (arr, sum) => arr.some(((set) => (n) => set.has(n) || !set.add(sum - n))(new Set()));
