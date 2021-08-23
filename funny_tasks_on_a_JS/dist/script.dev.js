2fd7bbbb9c30df7f9b45949b37865f931d42cc54
ruslan <sqwer1986@gmail.com>
2021-08-23 23:46:25 +0300

    copy

--- funny_tasks_on_a_JS/dist/script.dev.js
+++ funny_tasks_on_a_JS/dist/script.dev.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 /*
 Результатом декоратора debounce(f, ms) должна быть обёртка, 
 которая передаёт вызов f не более одного раза в ms миллисекунд. 
@@ -10,37 +12,62 @@
 и мы знаем, что повторный вызов в течение короткого промежутка времени не даст ничего нового. 
 Так что лучше не тратить на него ресурсы.
 */
-function debounce(f, ms) {
-  var detect = true;
-  return function (arg) {
-    if (detect) {
-      f(arg);
-      detect = false;
+// function debounce(f, ms) {
+//   let detect = true;
+//   return function (arg) {
+//     if (detect) {
+//       f(arg);
+//       detect = false;
+//     }
+//     setTimeout(() => (detect = true), ms);
+//   };
+// }
+// let f = debounce(console.log, 1000);
+// f(1); // выполняется немедленно
+// f(2); // проигнорирован
+// setTimeout(() => f(3), 100); // проигнорирован (прошло только 100 мс)
+// setTimeout(() => f(4), 1100); // выполняется
+// setTimeout(() => f(5), 1500); // проигнорирован (прошло только 400 мс от последнего вызова)
+// setTimeout(() => f(6), 2300); // выполняется
+var myObj = {
+  level: function level() {
+    return console.log('level 1');
+  },
+  hi: console.log('Hi 1'),
+  obj2: {
+    level: function level() {
+      return console.log('level 2');
+    },
+    hi2: console.log('Hi 2'),
+    'obj2-1': {
+      level: function level() {
+        return console.log('level 2-1');
+      },
+      'hi2-1': console.log('Hi 2-1')
+    },
+    'obj2-2': {
+      level: function level() {
+        return console.log('level 2-2');
+      },
+      hi22: console.log('Hi 2-2')
     }
+  }
+}; // console.log(myObj.obj2['obj2-2'].level());
 
-    setTimeout(function () {
-      return detect = true;
-    }, ms);
-  };
+function copy(obj) {
+  var result = {};
+
+  for (var key in obj) {
+    if (_typeof(obj[key]) === 'object' && !Array.isArray(obj[key])) {
+      result[key] = copy(obj[key]);
+    } else {
+      result[key] = obj[key];
+    }
+  }
+
+  return result;
 }
 
-var f = debounce(console.log, 1000);
-f(1); // выполняется немедленно
+console.log(myObj.obj2 === copy(myObj).obj2); // ! глубокое копирование объекта (функции тоже копирует как совершенно новые)
 
-f(2); // проигнорирован
-
-setTimeout(function () {
-  return f(3);
-}, 100); // проигнорирован (прошло только 100 мс)
-
-setTimeout(function () {
-  return f(4);
-}, 1100); // выполняется
-
-setTimeout(function () {
-  return f(5);
-}, 1500); // проигнорирован (прошло только 400 мс от последнего вызова)
-
-setTimeout(function () {
-  return f(6);
-}, 2300); // выполняется
+console.log(myObj.level === JSON.parse(JSON.stringify(myObj)).level);
