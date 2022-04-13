d5933cc219c2d570cb1620aaed7fd22d1dcfda10
ruslan <sqwer1986@gmail.com>
2022-04-14 01:34:54 +0300

    32....

--- funny_tasks_on_a_JS/script.js
+++ funny_tasks_on_a_JS/script.js
@@ -53,6 +53,12 @@
 При вызове калькулятора со строкой, которая не является математическим примером с одной из арифметических операций, описанных в требовании, приложение выбрасывает исключение и завершает свою работу.
 */
 function calculate(str) {
+    /*
+        TODO - Может быть вернуть просто драбление по split(' ')
+        TODO - усовершенствовать функцию decoding
+        TODO - 
+    */
+
     str = [...str].filter((el) => el !== ' ').join(''); // распарсим варианты когда строка может быть "1+1" или "1 + 1" или "1+ 1"
     
     const elements = [];
@@ -101,27 +107,25 @@ function calculate(str) {
         result = Number.isNaN(+elements[0]) ? decoding(elements[0]) / decoding(elements[2]) : +elements[0] / +elements[2];
     }
 
-    if (result < 0) return '';
-    if (result === 0) return 0;
-
-    return result;
+    if (result === 0 && Number.isNaN(+elements[0])) return '';
+    if (result === 0) return '0';
 
     result = Math.floor(result);
 
-    return Number.isNaN(+elements[0]) ? decoding(result) : result;
+    return Number.isNaN(+elements[0]) ? decoding(result).toString() : result.toString();
 }
 
-console.log(calculate('XCVIII + I')); // вернётся строка XCIX'
-// console.log(calculate('IX - VIII')); // вернётся строка I'
-// console.log(calculate('IX + VIII')); // вернётся строка XVII'
-// console.log(calculate('X * X')); // вернется строка 'C'
-// console.log(calculate('X + X')); // вернется строка 'XX'
-// console.log(calculate('1+1')); // вернется строка '2'
-// console.log(calculate('1 + 2')); // вернется строка '3'
-// console.log(calculate('VI / III')); // вернется строка 'II'
-// console.log(calculate('VII / III')); // вернётся строка II'
-// console.log(calculate('I + II')); // вернется строка 'III'
-// console.log(calculate('I - II')); // вернётся строка '' (пустая строка) т.к. в римской системе нет отрицательных чисел
-// console.log(calculate('I + 1')); // вернётся исключение (ошибка) throws Error т.к. используются одновременно разные системы счисления
-// console.log(calculate('I')); // вернётся исключение throws Error т.к. строка не является математической операцией
-// console.log(calculate('1 + 1 + 1')); // вернётся исключение throws Error т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)
+// console.log(calculate('XCVIII + I')); // вернётся строка XCIX'
+console.log(calculate('IX - VIII')); // вернётся строка I'
+console.log(calculate('IX + VIII')); // вернётся строка XVII'
+console.log(calculate('X * X')); // вернется строка 'C'
+console.log(calculate('X + X')); // вернется строка 'XX'
+console.log(calculate('1+1')); // вернется строка '2'
+console.log(calculate('1 + 2')); // вернется строка '3'
+console.log(calculate('VI / III')); // вернется строка 'II'
+console.log(calculate('VII / III')); // вернётся строка II'
+console.log(calculate('I + II')); // вернется строка 'III'
+console.log(calculate('I - II')); // вернётся строка '' (пустая строка) т.к. в римской системе нет отрицательных чисел
+console.log(calculate('I + 1')); // вернётся исключение (ошибка) throws Error т.к. используются одновременно разные системы счисления
+console.log(calculate('I')); // вернётся исключение throws Error т.к. строка не является математической операцией
+console.log(calculate('1 + 1 + 1')); // вернётся исключение throws Error т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)
