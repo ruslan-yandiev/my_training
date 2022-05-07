19beb88179e25ef2a163225ea60510262afee931
Ruslan <57350793+ruslan-yandiev@users.noreply.github.com>
2022-05-07 08:30:52 +0400

    39......

--- funny_tasks_on_a_JS/script.js
+++ funny_tasks_on_a_JS/script.js
@@ -48,3 +48,117 @@
 
 // video.showTags();
 
+/*
+Сравнить объекты с учетом вложенности.
+*/
+// function deepEqual(first, second) {
+//   let result = true;
+
+//   function check(a, b) {
+//     if (typeof a !== typeof b) result = false;
+//     if (a !== null && b !== null && typeof a === 'object') {
+//       if (Object.keys(a).length !== Object.keys(b).length) return false;
+//     } else if (a !== b) {
+//       result = false;
+//     }
+  
+//     for (let key in a) {
+//       if (typeof a[key] === typeof b[key] && a[key] !== null && b[key] !== null && typeof a[key] === "object") {
+//         check(a[key], b[key]);
+//       } else if (a[key] !== b[key]) {
+//         result = false;
+//       }
+//     }
+//   }
+
+//   check(first, second);
+
+//   return result;
+// }
+
+function deepEqual(f, s) {
+  let result = true;
+
+  function check(first, second) {
+    if (typeof first !== typeof second) {
+      result = false;
+      return;
+    };
+  
+    if (first !== null && second !== null && typeof first === 'object') {
+      if (Object.keys(first).length !== Object.keys(second).length) {
+        result = false;
+        return;
+      };
+
+      for (let key in first) {
+        if (typeof first[key] === typeof second[key]) {
+          if (first[key] !== null && second[key] !== null && typeof first[key] === "object") {
+            check(first[key], second[key]);
+          } else if (first[key] !== second[key]) {
+            result = false;
+            return;
+          }
+        } else {
+          result = false;
+          return;
+        }
+      }
+    } else if (first !== second) {
+      result = false;
+      return;
+    }
+  }
+
+  check(f, s);
+
+  return result;
+}
+
+const firstObject = {
+    a: {
+      b: {
+        c: 1,
+        d: 'string',
+        e: {
+          num: 1
+        }
+      }
+    }
+  };
+  
+  const secondObject = {
+    a: {
+      b: {
+        e: {
+          num: 1,
+        },
+        d: 'string',
+        c: 1,
+      }
+    }
+  };
+
+const a = {"name":"Misha","order":{"price":20,"count":1,"taxes":{"vat":{"name":"vat","amount":{"uah":10,"usd":0.37}}},"total":{"withoutTaxes":{"uah":20,"usd":0.74},"withTaxes":{"vat":{"uah":30,"usd":1.11}}}}};
+const b = {"name":"Misha","order":{"price":20,"count":1,"taxes":{"vat":{"name":"vat","amount":{"uah":10,"usd":0.37}}},"total":{"withoutTaxes":{"uah":20,"usd":0.74},"withTaxes":{"vat":{"uah":30,"usd":1.11,"eur":null}}}}};  
+
+const c = {'cc': 1, 'dd': {'cc': {'cc': 1, 'dd': 2}}};
+const d = {'cc': 1, 'dd': {'cc': {'cc': 1, 'dd': 2, 'ee': 3}}};
+
+const e = {'cc': 1, 'dd': {'cc': {'cc': 1, 'dd': 2}, 'a': {a: {}}}};
+const f = {'cc': 1, 'dd': {'cc': {'cc': 1, 'dd': 2}, 'a': {a: {}, b: 1}}};
+
+console.log(deepEqual(firstObject, secondObject)); // true
+console.log(deepEqual({ a:1, b: 3 }, { b: 2, a: 1})); // false
+console.log(deepEqual(1, 2)); // false
+console.log(deepEqual(true, false)); // false
+console.log(deepEqual(null, null)); // true
+console.log(deepEqual(null, 1)); // false
+console.log(deepEqual({}, null)); // false
+console.log(deepEqual(c, d)); // false
+console.log(deepEqual(e, f)); // false
+console.log(deepEqual(a, b)); // false
+
+
+
+
