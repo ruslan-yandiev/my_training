1a354909dff645dbcd7f5b321333a4a032cfa15b
Ruslan <57350793+ruslan-yandiev@users.noreply.github.com>
2022-09-06 08:15:58 +0300

    ....12

--- funny_tasks_on_a_JS/kata-a.js
+++ funny_tasks_on_a_JS/kata-a.js
@@ -1179,3 +1179,77 @@ console.log(errors); //   errors: [ExecutionError],
 console.log(errors[0].getArgData()); // '{{}'
 console.log(errors[1].stack); // '{{}'
 // * =============================================================================================================================
+/*
+SumFileSizes
+Напишите функцию, которая принимает имена двух файлов и вызывает функцию, переданную третьим параметром и передает ей первым агрументом сумму их размеров.
+
+Для получения рамзера файла необходимо использовать функцию getFileSize(filename, cb).
+*/
+let fileSizes = {
+  testFile1: 65,
+  testFile2: 48,
+};
+
+function getFileSize(filename, cb) {
+  setTimeout(() => cb(fileSizes[filename]), Math.random() * 500);
+}
+
+function sumFileSizes(filename1, filename2, cb) {
+  getFileSize(filename1, (arg) => {
+    getFileSize(filename2, (arg2) => {
+      cb(arg + arg2);
+    });
+  });
+}
+
+sumFileSizes("testFile1", "testFile2", (arg) => {
+  console.log(arg);
+});
+// * ==================================================================================================
+/*
+getUsersInfo
+Вам нужно написать функцию, которая получает массив всех пользователей и передает его в функцию коллбэк.
+
+Пример использования
+
+getUsersInfo((users) => {
+  console.log(users); // [ { name: 'Alex', age: 70 }, { name: 'Elon' } ]
+});
+Для получения данных вам предоставлены 2 асинхронные функции
+
+getUsersIds - Возвращает массив с идентификаторами пользователей
+getUserInfo - Возвращает данные пользователя по заданному идентификатору
+getUsersIds((ids) => {
+  console.log(ids); // ['id2', 'id6']
+});
+
+getUserInfo('someUserId', (userInfo) => {
+  console.log(userInfo); // { name: 'Alex', age: 70 }
+});
+Функция должна вызвать callback, переданный первым агрументом и передать туда массив данных о пользователях.
+
+Порядок пользователей в результирующем массиве должен соответствовать порядку идентификаторов в массиве из getUsersIds
+
+Hint: Вне платформы вы можете создать эти функции с помощью setTimeout и какого-то общего хранилица данных.
+*/
+const { getUserInfo, getUsersIds } = db;
+
+function getUsersInfo(onLoad) {
+  const result = [];
+
+  function callBack(arr, fn) {
+    if (arr.length > 0) {
+      fn(arr[0], (userInfo) => {
+        result.push(userInfo);
+        callBack(arr.slice(1), getUserInfo);
+      });
+    } else {
+      onLoad(result);
+    }
+  }
+
+  getUsersIds((ids) => {
+    callBack(ids, getUserInfo);
+  });
+}
+// * ========================================================================================================================
