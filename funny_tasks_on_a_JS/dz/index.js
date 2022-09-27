4688b07d85347fccdfe2a50421a9993a93bcc269
Ruslan <57350793+ruslan-yandiev@users.noreply.github.com>
2022-09-27 23:12:32 +0300

    123

--- funny_tasks_on_a_JS/dz/index.js
+++ funny_tasks_on_a_JS/dz/index.js
@@ -3,11 +3,12 @@ const textFild = document.querySelector(".text-fild");
 const resultCollection = document.querySelector(".result-collection");
 const autocomBoxItem = document.querySelectorAll(".autocom-box__item");
 
-let url = "https://api.github.com/search/repositories";
+let url = "https://api.github.com/search/repositories?q=";
+let repCollections;
 let id;
 let response;
 let json;
-let keyWord = "?q=";
+let keyWord = "";
 
 textFild.addEventListener("keydown", (event) => {
   if (id) clearTimeout(id);
@@ -18,8 +19,8 @@ textFild.addEventListener("keydown", (event) => {
       for (let i = 0; i < autocomBoxItem.length; i++) {
         autocomBoxItem[i].classList.add("hide");
       }
-
-      if (keyWord !== "?q=") {
+      console.log(url + keyWord);
+      if (url + keyWord !== "https://api.github.com/search/repositories?q=") {
         fetch(url + keyWord)
           .then((response) => {
             return response.json();
@@ -30,18 +31,20 @@ textFild.addEventListener("keydown", (event) => {
           .then((repCollection) => {
             repCollection?.forEach((el, index) => {
               autocomBoxItem[index].innerHTML = el.name;
+              autocomBoxItem[index].id = index;
               if (autocomBoxItem[index].classList.contains("hide")) {
                 autocomBoxItem[index].classList.remove("hide");
               }
             });
             console.log(repCollection);
             console.log(event.key);
+            repCollections = repCollection;
           })
           .catch((error) => {
             console.log(error);
           });
       }
-    }, 100);
+    }, 500);
   } catch (err) {
     alert("Ошибка HTTP: " + err.status);
   }
@@ -50,3 +53,38 @@ textFild.addEventListener("keydown", (event) => {
 // {}.name
 // {}.owner.login
 // {}.stargazers_count
+
+autocomBox.addEventListener("click", (event) => {
+  let elem = repCollections[event.target.id];
+  let resultCollection__item = document.createElement("div");
+  resultCollection__item.classList.add("result-collection__item");
+
+  let close = document.createElement("div");
+  close.classList.add("close", "result-collection__close");
+
+  let ul = document.createElement("ul");
+  ul.classList.add("list", "result-collection__element");
+
+  let li = document.createElement("li");
+  li.classList.add("list__item");
+  li.innerHTML = `Name: ${elem.name}`;
+  ul.append(li);
+  li = document.createElement("li");
+  li.classList.add("list__item");
+  li.innerHTML = `Owner: ${elem.owner.login}`;
+  ul.append(li);
+  li = document.createElement("li");
+  li.classList.add("list__item");
+  li.innerHTML = `Stars: ${elem.stargazers_count}`;
+  ul.append(li);
+  resultCollection__item.append(ul);
+  resultCollection__item.append(close);
+  resultCollection.append(resultCollection__item);
+});
+
+resultCollection.addEventListener("click", (event) => {
+  console.log(event);
+  if (event.target.classList.contains("close")) {
+    event.target.parentElement.remove();
+  }
+});
