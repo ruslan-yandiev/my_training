c01b78daa2ed45eef3d72d3598b92f19612430fa
Ruslan <57350793+ruslan-yandiev@users.noreply.github.com>
2022-10-05 16:31:51 +0300

    Практическая задача

--- funny_tasks_on_a_JS/dz/index.js
+++ funny_tasks_on_a_JS/dz/index.js
@@ -6,54 +6,13 @@ const autocomBoxItem = document.querySelectorAll(".autocom-box__item");
 let url = "https://api.github.com/search/repositories?q=";
 let repCollections;
 let id;
-let response;
-let json;
-let keyWord = "";
 
-textFild.addEventListener("keydown", (event) => {
+// ===========================================================================================================
+textFild.addEventListener("input", (event) => {
   if (id) clearTimeout(id);
-  event.key === "Backspace" ? (keyWord = keyWord.substring(0, keyWord.length - 1)) : (keyWord += event.key);
-
-  try {
-    id = setTimeout(async () => {
-      for (let i = 0; i < autocomBoxItem.length; i++) {
-        autocomBoxItem[i].classList.add("hide");
-      }
-      console.log(url + keyWord);
-      if (url + keyWord !== "https://api.github.com/search/repositories?q=") {
-        fetch(url + keyWord)
-          .then((response) => {
-            return response.json();
-          })
-          .then((json) => {
-            return json.items?.filter((el, index) => index < 5);
-          })
-          .then((repCollection) => {
-            repCollection?.forEach((el, index) => {
-              autocomBoxItem[index].innerHTML = el.name;
-              autocomBoxItem[index].id = index;
-              if (autocomBoxItem[index].classList.contains("hide")) {
-                autocomBoxItem[index].classList.remove("hide");
-              }
-            });
-            console.log(repCollection);
-            console.log(event.key);
-            repCollections = repCollection;
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
-    }, 500);
-  } catch (err) {
-    alert("Ошибка HTTP: " + err.status);
-  }
+  send(url + textFild.value);
 });
-
-// {}.name
-// {}.owner.login
-// {}.stargazers_count
-
+// ===========================================================================================================
 autocomBox.addEventListener("click", (event) => {
   let elem = repCollections[event.target.id];
   let resultCollection__item = document.createElement("div");
@@ -80,11 +39,52 @@ autocomBox.addEventListener("click", (event) => {
   resultCollection__item.append(ul);
   resultCollection__item.append(close);
   resultCollection.append(resultCollection__item);
-});
 
+  textFild.value = "";
+  cleaning();
+});
+// ===========================================================================================================
 resultCollection.addEventListener("click", (event) => {
-  console.log(event);
   if (event.target.classList.contains("close")) {
     event.target.parentElement.remove();
   }
 });
+// ===========================================================================================================
+function cleaning() {
+  for (let i = 0; i < autocomBoxItem.length; i++) {
+    autocomBoxItem[i].classList.add("hide");
+  }
+}
+
+function send(fullURL) {
+  try {
+    id = setTimeout(async () => {
+      cleaning();
+
+      if (fullURL !== url) {
+        fetch(fullURL)
+          .then((response) => {
+            return response.json();
+          })
+          .then((json) => {
+            return json.items?.filter((el, index) => index < 5);
+          })
+          .then((repCollection) => {
+            repCollection?.forEach((el, index) => {
+              autocomBoxItem[index].innerHTML = el.name;
+              autocomBoxItem[index].id = index;
+              if (autocomBoxItem[index].classList.contains("hide")) {
+                autocomBoxItem[index].classList.remove("hide");
+              }
+            });
+            repCollections = repCollection;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    }, 300);
+  } catch (err) {
+    alert("Ошибка HTTP: " + err.status);
+  }
+}
