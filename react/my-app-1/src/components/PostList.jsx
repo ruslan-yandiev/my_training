13dda51bc8d67513bcef1d7efadfb0e0a280659d
Ruslan <57350793+ruslan-yandiev@users.noreply.github.com>
2022-10-22 08:13:36 +0300

    ......

--- react/my-app-1/src/components/PostList.jsx
+++ react/my-app-1/src/components/PostList.jsx
@@ -1,22 +1,17 @@
 import React from "react";
 import PostItem from "./PostItem";
 
+const PostList = ({ posts, title }) => {
+  return (
+    //! В компоненте может быть только один корневой элемент
+    <div>
+      <h1 style={{ textAlign: "center" }}>{title}</h1>
 
-const PostList = ({posts, title}) => {
-
-
-    return (
-        //! В компоненте может быть только один корневой элемент
-        <div>
-            <h1 style={{textAlign: 'center'}}>
-            {title}
-            </h1>
-
-            {posts.map((post) => 
-                <PostItem post={post} key={post.id}/>
-            )}
-        </div>
-    );
-}
+      {posts.map((post) => (
+        <PostItem post={post} key={post.id} />
+      ))}
+    </div>
+  );
+};
 
 export default PostList;
