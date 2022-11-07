c5f8686a13bb795b7c3d546981c14ed2ae201713
Ruslan <57350793+ruslan-yandiev@users.noreply.github.com>
2022-11-07 08:43:45 +0300

    .......1

--- react/new-app/src/App.js
+++ react/new-app/src/App.js
@@ -4,12 +4,14 @@ import React, { useState } from "react";
 import PostList from "./components/PostList";
 import "./styles/App.css";
 import TestsList from "./components/TestsList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: "javascript", body: "Description" },
     { id: 2, title: "JS", body: "Description" },
-    { id: 3, title: "JS", body: "Description" },
+    { id: 3, title: "SQL", body: "Description" },
   ]);
 
   const tests = [
@@ -18,16 +20,25 @@ function App() {
     { id: 3, title: "HTML", body: ["HTML - 1", "HTML - 2", "HTML - 3"], info: "Toma" },
   ];
 
-  const [posts2, setPosts2] = useState([
-    { id: 1, title: "Ruby", body: "Description" },
-    { id: 2, title: "Ruby", body: "Description" },
-    { id: 3, title: "Ruby", body: "Description" },
-  ]);
+  function addNewPost() {
+    posts.push({ id: posts[posts.length - 1].id + 1, title: namePost, body: bodyPost });
+    setPosts(posts);
+  }
+
+  const [namePost, setNamePost] = useState("");
+  const [bodyPost, setBodyPost] = useState("");
 
   return (
     <div className="App">
-      <PostList posts={posts2} title={"Посты про JS"} />
-      <PostList posts={posts2} title={"Посты про Ruby"} />
+      <form>
+        <MyInput type="text" placeholder="Название поста" value={namePost} onChange={(event) => setNamePost(event.target.value)} />
+        <MyInput type="text" placeholder="Описание поста" value={bodyPost} onChange={(event) => setBodyPost(event.target.value)} />
+        <MyButton onClick={addNewPost} disabled>
+          Create post
+        </MyButton>
+      </form>
+
+      <PostList posts={posts} />
       <TestsList tests={tests} />
     </div>
   );
