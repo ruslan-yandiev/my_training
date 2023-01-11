6a81e32fcb1cba3bd599b673b405652fcde811de
Ruslan <57350793+ruslan-yandiev@users.noreply.github.com>
2023-01-11 23:29:36 +0300

    ...2

---
+++ react/todo/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Привет руслан</h1>
+    </div>
+  );
+}
+
+export default App;
