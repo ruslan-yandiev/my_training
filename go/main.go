b4f10280093fb368582f3e6cd7ae197a7d01bddd
Ruslan <57350793+ruslan-yandiev@users.noreply.github.com>
2023-05-16 22:06:25 +0300

    ..12

--- go/main.go
+++ go/main.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
+// точка входа самозапускающаяся, не должна принимать никакие аргументы (и не возвращает вроде бы...)
 func main() {
-	fmt.Println("Hello world")
+	arr := make([]int, 0)
+
+	for i := 0; i < 10; i++ {
+		arr = append(arr, i)
+	}
+
+	fmt.Println(arr)
+
+	main2()
+}
+
+func main2() {
+	engToRus := make(map[string]string, 0)
+
+	engToRus["Ruslan"] = "Yandiev"
+	engToRus["Ivan"] = "Ivanov"
+	engToRus["keyyy"] = "kuuu"
+
+	delete(engToRus, "keyyy")
+
+	for key, value := range engToRus {
+		fmt.Println("key:", key, "value:", value)
+	}
 }
