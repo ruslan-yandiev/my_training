9473d9659d71c9898618d229f0ff9c31513388e6
Ruslan <57350793+ruslan-yandiev@users.noreply.github.com>
2023-06-14 23:06:54 +0300

    ..2

--- go/test.go
+++ go/test.go
@@ -5,16 +5,9 @@ import (
 )
 
 func main() {
+	fmt.Println(GenerateSelfStory("Ruslan", 37, 100000000000.1000043))
+}
 
-	str := "wwwww"
-	arr := []string{str}
-	arr = append(arr, "qqqq", "zzzzzz")
-	str2 := ""
-
-	for i := 0; i < len(arr); i++ {
-		str2 += arr[i]
-		str2 += " "
-	}
-
-	fmt.Println(str2)
+func GenerateSelfStory(name string, age int, money float64) string {
+	return fmt.Sprintf("Hello! My name is %s. I'm %d y.o. And I also have $%.2f in my wallet right now.", name, age, money)
 }
