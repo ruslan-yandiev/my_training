020bf68622cf83ff21e52979b5a0cdb417a11300
Ruslan <57350793+ruslan-yandiev@users.noreply.github.com>
2023-07-04 23:27:06 +0300

    ...1.1.1

--- go/test.go
+++ go/test.go
@@ -1,31 +1,9 @@
 package main
 
-import "fmt"
-
-type Person struct {
-	Age uint8
-}
-
-type PersonList []Person
-
 func main() {
-	pl := PersonList{
-		{Age: 18},
-		{Age: 44},
-		{Age: 18},
-		{Age: 18},
+	var t interface {
+		talk() string
 	}
 
-	fmt.Println(pl.GetAgePopularity()) // map[18:2 44:1]
-	// pl.GetAgePopularity() // map[18:2 44:1]
-}
-
-func (pl PersonList) GetAgePopularity() map[uint8]int {
-	result := make(map[uint8]int, len(pl))
-
-	for _, v := range pl {
-		result[v.Age]++
-	}
-
-	return result
+	type martian struct{}
 }
