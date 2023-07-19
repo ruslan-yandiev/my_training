e1d87eecf1eaec59b7bb5f6c1c863e6f23a48661
Ruslan <57350793+ruslan-yandiev@users.noreply.github.com>
2023-07-19 08:19:35 +0300

    ...32

--- funny_tasks_on_a_JS/script.js
+++ funny_tasks_on_a_JS/script.js
@@ -2,18 +2,16 @@ function quickSort(arr) {
   const mid = Math.floor(arr.length / 2);
   let pivotIndex = mid;
   let rightIndex = arr.length - 1;
-  let leftIndex = mid;
+  let leftIndex = 0;
 
-  for (;pivotIndex > 0;) {
-    for (let i = 0; i <= pivotIndex; i++) {
+  for (; pivotIndex > 0; ) {
+    for (let i = leftIndex; i <= pivotIndex; i++) {
       if (arr[i] < pivotIndex) continue;
 
       for (j = pivotIndex; j < rightIndex; j++) {
         if (arr[i] > arr[j]) [arr[i], arr[j]] = [arr[j], arr[i]];
       }
     }
-
-    for (let i = leftIndex; i < arr)
 
     rightIndex = pivotIndex;
     pivotIndex = Math.floor(pivotIndex / 2);
