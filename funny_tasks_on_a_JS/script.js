183ffe6201f95e9d25cdb69a82c172f184ee515d
Ruslan <57350793+ruslan-yandiev@users.noreply.github.com>
2023-08-05 01:09:56 +0300

    ...1

--- funny_tasks_on_a_JS/script.js
+++ funny_tasks_on_a_JS/script.js
@@ -1,6 +1,4 @@
-function quickSort(arrr) {
-    
-}
+function quickSort(arr) {}
 
 console.log(quickSort([11, 4, 8, 15, 1, 0, 11, 3, 6, 22, 11, 1, 4, 8, 15]));
 console.log(quickSort([5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 5, 5, 4, 3, 2, 1, 0]));
--- go/test.go
+++ go/test.go
@@ -9,31 +9,35 @@ func quickSort(arr []int) []int {
 		return arr
 	}
 
-	pivotIndex := len(arr) / 2
-	pivot := arr[pivotIndex]
-	min, max := make([]int, 0, len(arr)/2), make([]int, 0, len(arr)/2)
+	pivIndex := len(arr) / 2
+	piv := arr[pivIndex]
+	left := make([]int, 0, pivIndex)
+	right := make([]int, 0, pivIndex)
 
 	for i := 0; i < len(arr); i++ {
-		if i == pivotIndex {
+		if i == pivIndex {
 			continue
 		}
 
-		if arr[i] < pivot {
-			min = append(min, arr[i])
+		if arr[i] < piv {
+			left = append(left, arr[i])
 		} else {
-			max = append(max, arr[i])
+			right = append(right, arr[i])
 		}
 	}
 
 	result := make([]int, 0, len(arr))
-
-	result = append(result, quickSort(min)...)
-	result = append(result, pivot)
-	result = append(result, quickSort(max)...)
-
+	result = append(result, quickSort(left)...)
+	result = append(result, piv)
+	result = append(result, quickSort(right)...)
 	return result
 }
 
 func main() {
 	fmt.Println(quickSort([]int{4, 8, 15, 1, 0, 11, 3, 6, 22, 1, 4, 8, 15}))
+	fmt.Println(quickSort([]int{11, 4, 8, 15, 1, 0, 11, 3, 6, 22, 11, 1, 4, 8, 15}))
+	fmt.Println(quickSort([]int{5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 5, 5, 4, 3, 2, 1, 0}))
+	fmt.Println(quickSort([]int{5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5}))
+	fmt.Println(quickSort([]int{0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5}))
+	fmt.Println(quickSort([]int{0, 1, 2, 3, -6, 14, 4, 5, 0, 1, -33, 2, 3, 4, 5}))
 }
