6ddaca88b55434f4a76df350d3d1d9b993a4e244
Ruslan <57350793+ruslan-yandiev@users.noreply.github.com>
2023-09-17 22:08:23 +0300

    ...ee

--- go/test.go
+++ go/test.go
@@ -4,59 +4,18 @@ import (
 	"fmt"
 )
 
-func quickSort(arr []int) []int {
-	if len(arr) > 1 {
-		mid := len(arr) / 2
-		left := []int{}
-		right := []int{}
+func myF(num int) int {
+	accum := 0
 
-		for i := 0; i < len(arr); i++ {
-			if i < mid {
-				left = append(left, arr[i])
-			} else {
-				right = append(right, arr[i])
-			}
-		}
-
-		quickSort(left)
-		quickSort(right)
-
-		l, r, k := 0, 0, 0
-
-		for l < len(left) && r < len(right) {
-			if left[l] <= right[r] {
-				arr[k] = left[l]
-				l++
-			} else {
-				arr[k] = right[r]
-				r++
-			}
-
-			k++
-		}
-
-		for l < len(left) {
-			arr[k] = left[l]
-			l++
-			k++
-		}
-
-		for r < len(right) {
-			arr[k] = right[r]
-			r++
-			k++
+	for i := 0; i <= num; i++ {
+		if i%3 == 0 && i%5 == 0 && i%7 == 0 {
+			accum += i
 		}
 	}
 
-	return arr
+	return accum
 }
 
 func main() {
-	// fmt.Println(quickSort([]int{2, 1}))
-	fmt.Println(quickSort([]int{4, 8, 15, 1, 0, 11, 3, 6, 22, 1, 4, 8, 15}))
-	fmt.Println(quickSort([]int{11, 4, 8, 15, 1, 0, 11, 3, 6, 22, 11, 1, 4, 8, 15}))
-	fmt.Println(quickSort([]int{5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 5, 5, 4, 3, 2, 1, 0}))
-	fmt.Println(quickSort([]int{5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5}))
-	fmt.Println(quickSort([]int{0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5}))
-	fmt.Println(quickSort([]int{0, 1, 2, 3, -6, 14, 4, 5, 0, 1, -33, 2, 3, 4, 5}))
+	fmt.Println(myF(70))
 }
---
+++ алгоритмы/сортировки/divide_and_conquer(разделяй_и_властвуй)/sort_merge.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+)
+
+func quickSort(arr []int) []int {
+	if len(arr) > 1 {
+		mid := len(arr) / 2
+		left := make([]int, 0, mid+1)
+		right := make([]int, 0, mid+1)
+
+		for i := 0; i < len(arr); i++ {
+			if i < mid {
+				left = append(left, arr[i])
+			} else {
+				right = append(right, arr[i])
+			}
+		}
+
+		quickSort(left)
+		quickSort(right)
+
+		l, r, k := 0, 0, 0
+
+		for l < len(left) && r < len(right) {
+			if left[l] <= right[r] {
+				arr[k] = left[l]
+				l++
+			} else {
+				arr[k] = right[r]
+				r++
+			}
+			k++
+		}
+
+		for l < len(left) {
+			arr[k] = left[l]
+			l++
+			k++
+		}
+
+		for r < len(right) {
+			arr[k] = right[r]
+			r++
+			k++
+		}
+	}
+
+	return arr
+}
+
+func main() {
+	fmt.Println(quickSort([]int{4, 8, 15, 1, 0, 11, 3, 6, 22, 1, 4, 8, 15}))
+	fmt.Println(quickSort([]int{11, 4, 8, 15, 1, 0, 11, 3, 6, 22, 11, 1, 4, 8, 15}))
+	fmt.Println(quickSort([]int{5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 5, 5, 4, 3, 2, 1, 0}))
+	fmt.Println(quickSort([]int{5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5}))
+	fmt.Println(quickSort([]int{0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5}))
+	fmt.Println(quickSort([]int{0, 1, 2, 3, -6, 14, 4, 5, 0, 1, -33, 2, 3, 4, 5}))
+}
