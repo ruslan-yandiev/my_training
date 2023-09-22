dc7c3090cd2b615e8bc5d55c795feca7b7d3109e
Ruslan <57350793+ruslan-yandiev@users.noreply.github.com>
2023-09-22 08:18:06 +0300

    112e

--- funny_tasks_on_a_JS/script.js
+++ funny_tasks_on_a_JS/script.js
@@ -1,3 +1,24 @@
-function func(num) {}
+function func(str) {
+  const obj = {
+    A: "a",
+    B: "b",
+    C: "c",
+    D: "d",
+    E: "e",
+    F: "f",
+  };
 
-console.log(func(70));
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (obj[str[i]]) {
+      result += obj[str[i]];
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
+}
+
+console.log(func("eeEEfFFFaAAA  1212`e ppp"));
