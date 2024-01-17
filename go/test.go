22d9b163b986d49ddafaf8b0591c9d7d2a075d8b
ruslan-yandiev <sqwer1986@sqwer1986.com>
2024-01-17 08:34:00 +0300

    ...d

--- go/test.go
+++ go/test.go
@@ -4,16 +4,32 @@ import (
 	"fmt"
 )
 
-func bableSort(arr []int) []int {
-	for i := 1; i < len(arr); i++ {
-		for j := i; j > 0 && arr[j] < arr[j-1]; j-- {
-			arr[j], arr[j-1] = arr[j-1], arr[j]
+func qweekSort(arr []int) []int {
+	if len(arr) < 1 {
+		return arr
+	}
+
+	index := len(arr) / 2
+	pointe := arr[index]
+	left := []int{}
+	right := []int{}
+
+	for i := 0; i < len(arr); i++ {
+		if i != index && arr[i] < pointe {
+			left = append(left, arr[i])
+		} else if i != index {
+			right = append(right, arr[i])
 		}
 	}
 
-	return arr
+	newArr := []int{}
+	newArr = append(newArr, qweekSort(left)...)
+	newArr = append(newArr, pointe)
+	newArr = append(newArr, qweekSort(right)...)
+
+	return newArr
 }
 
 func main() {
-	fmt.Println(bableSort([]int{-22, 14, 1, -33, 2, 11, 2, 14, -4, 5, 2, 0}))
+	fmt.Println(qweekSort([]int{-22, 14, 1, -33, 2, 11, 2, 14, -4, 5, 2, 0}))
 }
