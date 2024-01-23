4c028abbeefad3e4c80c6e7fd7db42fba71a6965
ruslan-yandiev <sqwer1986@sqwer1986.com>
2024-01-23 03:17:33 +0300

    ke

--- go/test.go
+++ go/test.go
@@ -4,8 +4,40 @@ import (
 	"fmt"
 )
 
-func bableSort(arr []int) []int {
+func mergeSort(arr []int) []int {
+	if len(arr) > 1 {
+		mid := len(arr) / 2
+		left := mergeSort(arr[0:mid])
+		right := mergeSort(arr[mid:])
 
+		var result []int
+
+		l, r := 0, 0
+
+		for l < len(left) && r < len(right) {
+			if left[l] <= right[r] {
+				result = append(result, left[l])
+				l++
+			} else {
+				result = append(result, right[r])
+				r++
+			}
+		}
+
+		for l < len(left) {
+			result = append(result, left[l])
+			l++
+		}
+
+		for r < len(right) {
+			result = append(result, right[r])
+			r++
+		}
+
+		return result
+	}
+
+	return arr
 }
 
 func main() {
--- алгоритмы/сортировки/divide_and_conquer(разделяй_и_властвуй)/sort_merge.go
+++ алгоритмы/сортировки/divide_and_conquer(разделяй_и_властвуй)/sort_merge.go
@@ -61,7 +61,7 @@ func main() {
 }
 
 // ===================================================================================
-// ! имутабельный способ. переданный массив не будет изменен, вместо ӕтого вернется новый отсортированный массив.
+// ! имутабельные способы переданный массив не будет изменен, вместо ӕтого вернется новый отсортированный массив.
 func mergeSort(arr []int) []int {
 	if len(arr) <= 1 {
 		return arr
@@ -99,4 +99,45 @@ func merge(left, right []int) []int {
 
 func main() {
 	fmt.Println(mergeSort([]int{-22, 14, 1, -33, 2, 11, 2, 14, -4, 5, 2, 0}))
+}
+
+// =================================================================================================
+func mergeSort(arr []int) []int {
+	if len(arr) > 1 {
+		mid := len(arr) / 2
+		left := mergeSort(arr[0:mid])
+		right := mergeSort(arr[mid:])
+
+		var result []int
+
+		l, r := 0, 0
+
+		for l < len(left) && r < len(right) {
+			if left[l] <= right[r] {
+				result = append(result, left[l])
+				l++
+			} else {
+				result = append(result, right[r])
+				r++
+			}
+		}
+
+		for l < len(left) {
+			result = append(result, left[l])
+			l++
+		}
+
+		for r < len(right) {
+			result = append(result, right[r])
+			r++
+		}
+
+		return result
+	}
+
+	return arr
+}
+
+func main() {
+	fmt.Println(mergeSort([]int{-22, 14, 1, -33, 2, 11, 2, 14, -4, 5, 2, 0})) // [-33 -22 -4 0 1 2 2 2 5 11 14 14]
 }
